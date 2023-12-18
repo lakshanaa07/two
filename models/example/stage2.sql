8b23329cda4d1c0eb483811fde7995f42dade7a6
lakshanaa07 <148964858+lakshanaa07@users.noreply.github.com>
2023-12-18 11:53:39 +0530

    Update stage2.sql

--- models/example/stage2.sql
+++ models/example/stage2.sql
@@ -2,5 +2,5 @@ with
 data as(
 Select * from `cloudside-academy.lakshana_test.annual_enterprise_survey02`),
 B as(
-Select year, industry_name_ANZSIC, variable, value from data)
+Select year, Industry_name_NZSIOC, variable, value from data)
 Select * from B
